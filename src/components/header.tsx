424e57ca9ef7691aedc322b954d8f06e58a44213
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-08-23 20:57:26 +0200

    Refactor header into separate component
    Extracted the header section from Home.tsx into a new Header component for better modularity and reusability. Updated CSS and SCSS selectors from .home to .header to match the new structure.

---
+++ src/components/header.tsx
@@ -0,0 +1,38 @@
+import '../styles/style.css'
+import { useNavigate } from 'react-router-dom'
+
+const Header = () => {
+    const navigate = useNavigate(); // Initialize the navigate function
+
+  // Function to handle button click and redirect to home page
+  const handleBookRoomClick = () => {
+    navigate('/'); // Redirects to the home page
+  }
+
+  return (
+    <header className='header'>
+      <div className="divL">
+        <button className="name"
+            onClick={handleBookRoomClick} // Add the onClick handler
+        >
+            Dankoo Baba & ETC Guesthouse
+        </button>
+      </div>
+
+      <div className="divR">
+        <button className='RBtn'>About us</button>
+        <button className='RBtn'>Services</button>
+      </div>
+
+      <div className="bookSection">
+        <button
+          className="inline-block rounded-sm cursor-pointer border border-indigo-300 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:ring-3 focus:outline-hidden"
+        >
+          Book a room
+        </button>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,12 @@
+import Header from '@/components/header'
 import '../styles/style.css'
-import { useNavigate } from 'react-router-dom' // Add this import
 
 const Home = () => {
-  const navigate = useNavigate(); // Initialize the navigate function
-
-  // Function to handle button click and redirect to home page
-  const handleBookRoomClick = () => {
-    navigate('/'); // Redirects to the home page
-  }
 
   return (
-    <div className="home">
-      <div className="divL">
-        <button className="name"
-            onClick={handleBookRoomClick} // Add the onClick handler
-        >
-            Dankoo Baba & ETC Guesthouse
-        </button>
-      </div>
-
-      <div className="divR">
-        <button className='RBtn'>About us</button>
-        <button className='RBtn'>Services</button>
-      </div>
-
-      <div className="bookSection">
-        <button
-          className="inline-block rounded-sm cursor-pointer border border-indigo-300 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:ring-3 focus:outline-hidden"
-        >
-          Book a room
-        </button>
-      </div>
-    </div>
+    <>
+     <Header /> 
+    </>
   )
 }
 
