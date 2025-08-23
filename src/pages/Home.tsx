3de5d6960713c3e1efad7986dcb154a49e24a697
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-08-23 20:34:19 +0200

    Add homepage header layout and PT Serif font
    Introduced a new header layout on the homepage with left, right, and booking sections. Added PT Serif font via Google Fonts, updated styles in SCSS/CSS for layout and button interactions, and adjusted the background and border for a cleaner look.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,20 @@ import '../styles/style.css'
 const Home = () => {
   return (
     <div className="home">
-      
+      <div className="divL">
+        <p className="name">Dankoo Baba & ETC Guesthouse</p>
+      </div>
+
+      <div className="divR">
+        <button className='RBtn'>About us</button>
+        <button className='RBtn'>Services</button>
+      </div>
+
+      <div className="bookSection">
+        <button className="inline-block rounded-sm cursor-pointer border border-indigo-300 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:ring-3 focus:outline-hidden" >
+            Book a room
+        </button>
+      </div>
     </div>
   )
 }
