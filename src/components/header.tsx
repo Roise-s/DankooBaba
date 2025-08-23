7e4ff20e969ec92e632684fce149131b146f2671
Kgaugelo Tladi <milkeyroise@gmail.com>
2025-08-23 21:07:36 +0200

    Add MainHome component and update Home page layout
    Introduces a new MainHome component with a welcome section and call-to-action buttons. Updates Home.tsx to use MainHome, refines header component, and adjusts styles in style.css, style.scss, and their map files for improved layout and appearance.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -19,16 +19,11 @@ const Header = () => {
         </button>
       </div>
 
-      <div className="divR">
-        <button className='RBtn'>About us</button>
-        <button className='RBtn'>Services</button>
-      </div>
-
       <div className="bookSection">
         <button
           className="inline-block rounded-sm cursor-pointer border border-indigo-300 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:ring-3 focus:outline-hidden"
         >
-          Book a room
+          Services
         </button>
       </div>
     </header>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Header from '@/components/header'
 import '../styles/style.css'
+import MainHome from '@/components/MainHome'
 
 const Home = () => {
 
   return (
     <>
-     <Header /> 
+     <Header />
+     <MainHome /> 
     </>
   )
 }
